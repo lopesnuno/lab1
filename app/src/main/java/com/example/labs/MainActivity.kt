109876a5231b87558a6303d53687e40ffcf65010
lopesnuno <lopesnuno@ipvc.pt>
2024-03-14 22:26:23 +0000

    Change language

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var buttonPT: Button
+    private lateinit var buttonENG: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         button = findViewById(R.id.button);
+        buttonPT = findViewById(R.id.ptButton);
+        buttonENG = findViewById(R.id.engButton);
 
         button.setOnClickListener {
             val intent = Intent(this, AT2::class.java)
             startActivity(intent)
         }
+
+        buttonPT.setOnClickListener {
+            setLanguage("pt")
+        }
+
+        buttonENG.setOnClickListener {
+            setLanguage("eng")
+        }
+    }
+
+    private fun setLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val configuration = resources.configuration
+        configuration.setLocale(locale)
+
+        val context = createConfigurationContext(configuration)
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+
+        recreate() // Recreate the activity to reflect the change
     }
 }
